62bebd31b75b87a52f19f2952ff24e106a469563
1gtm <1gtm@appscode.com>
2021-10-20 08:44:07 +0000

    Generate code for provider@v1.1.1-0.20210727130052-a55db9bd1897 gen@6f89580
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- apis/user/v1alpha1/zz_generated.deepcopy.go
+++ apis/user/v1alpha1/zz_generated.deepcopy.go
@@ -103,6 +103,11 @@ func (in *ApiKeySpec) DeepCopyInto(out *ApiKeySpec) {
 		*out = new(v1.LocalObjectReference)
 		**out = **in
 	}
+	if in.BackendRef != nil {
+		in, out := &in.BackendRef, &out.BackendRef
+		*out = new(v1.LocalObjectReference)
+		**out = **in
+	}
 	return
 }
 
